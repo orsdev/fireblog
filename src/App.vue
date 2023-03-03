742893ce838afd0459a5e2093186e0f57cce1846
ogundare samuel <oluwadareysamuel@gmail.com>
2023-03-03 08:43:18 +0100

    feat: initial setup

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Lorem ipsum</h1>
+  </div>
+</template>
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        xs: "475px",
+        ...defaultTheme.screens,
+      },
+    },
+  },
+  plugins: [],
+};
